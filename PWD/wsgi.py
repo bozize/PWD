10a0d162b5df6cf956a05e5ac7b52395f04e6c7b
boziz <bozize@gmail.com>
2024-10-18 22:15:21 +0300

    fix

--- PWD/wsgi.py
+++ PWD/wsgi.py
@@ -14,24 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PWD.settings')
 
 application = get_wsgi_application()
-
-
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-# Hardcoded credentials
-username = 'admin'
-email = 'admin@example.com'
-password = '0711169847a'
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(
-        username=username,
-        email=email,
-        password=password
-    )
-    print("Superuser created.")
-else:
-    print("Superuser already exists.")
