a41141d696c0ec6d97cfe1228b15fcb7b5a52795
boziz <bozize@gmail.com>
2024-10-23 04:28:22 +0300

    adited readme

--- pwdapp/urls.py
+++ pwdapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from .views import upload_violation
+
 
 urlpatterns = [
     path('process-violation/', views.process_violation, name='process_violation'),
     path('pwd/<str:mac_address>/', views.get_pwd_by_mac, name='get_pwd_by_mac'),  # Updated to use mac_address
-    path('upload-violation/', upload_violation, name='upload_violation'),
 ]
